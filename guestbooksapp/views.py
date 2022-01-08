2f796c94837dfa7859dab72a1860bad140ebff16
Farhatjan <faralost@gmail.com>
2022-01-08 12:15:19 +0600

    added list view of records in index page

--- guestbooksapp/views.py
+++ guestbooksapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from guestbooksapp.models import GuestBookRecord
+
 
 def index(request):
-    return render(request, 'index.html')
+    records = GuestBookRecord.objects.all()
+    return render(request, 'index.html', {'records': records})
