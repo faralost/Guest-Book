abc55f14a1ec0ee8a85f2ca579bc217abf1de0f6
Farhatjan <faralost@gmail.com>
2022-01-08 12:34:30 +0600

    added ability to add new records via form model

--- guestbooksapp/urls.py
+++ guestbooksapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from guestbooksapp.views import index
+from guestbooksapp.views import index, record_add
 
 urlpatterns = [
     path('', index, name='index'),
+    path('records/add/', record_add, name='record_add')
 ]
--- guestbooksapp/views.py
+++ guestbooksapp/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from guestbooksapp.forms import RecordForm
 from guestbooksapp.models import GuestBookRecord
 
 
 def index(request):
     records = GuestBookRecord.objects.all()
     return render(request, 'index.html', {'records': records})
+
+
+def record_add(request):
+    if request.method == 'GET':
+        form = RecordForm()
+        return render(request, 'record_add.html', {'form': form})
+    else:
+        form = RecordForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        return render(request, 'record_add.html', {'form': form})
