169984d7cba1fc668a092d4fb419473de7014f0e
Farhatjan <faralost@gmail.com>
2022-01-08 11:45:46 +0600

    added GuestBookRecord to admin panel

--- guestbooksapp/admin.py
+++ guestbooksapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from guestbooksapp.models import GuestBookRecord
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'email', 'status', 'created_at']
+    list_filter = ['author', 'status']
+    search_fields = ['author']
+    fields = ['author', 'text', 'email', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(GuestBookRecord, RecordAdmin)
