a8b42fd2349ef585562d9777e4f7e918d69bdc32
Farhatjan <faralost@gmail.com>
2022-01-08 11:44:57 +0600

    added GuestBookRecord model

--- guestbooksapp/models.py
+++ guestbooksapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class GuestBookRecord(models.Model):
+
+    STATUS_CHOICES = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+    author = models.CharField(max_length=50, verbose_name='Автор')
+    email = models.EmailField(max_length=50, verbose_name='Email')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='active', verbose_name='Статус')
+
+    def __str__(self):
+        return f"{self.pk}. {self.author}: {self.status}"
+
+    class Meta:
+        db_table = 'records'
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
