846a816f3721f4f67e3bebfef588fb2adc944c37
Farhatjan <faralost@gmail.com>
2022-01-08 12:44:04 +0600

    added ability to update our records

--- guestbooksapp/urls.py
+++ guestbooksapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from guestbooksapp.views import index, record_add
+from guestbooksapp.views import index, record_add, record_update
 
 urlpatterns = [
     path('', index, name='index'),
-    path('records/add/', record_add, name='record_add')
+    path('records/add/', record_add, name='record_add'),
+    path('record/<int:pk>/update/', record_update, name='record_update'),
 ]
--- guestbooksapp/views.py
+++ guestbooksapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from guestbooksapp.forms import RecordForm
 from guestbooksapp.models import GuestBookRecord
@@ -19,3 +19,23 @@ def record_add(request):
             form.save()
             return redirect('index')
         return render(request, 'record_add.html', {'form': form})
+
+
+def record_update(request, pk):
+    record = get_object_or_404(GuestBookRecord, pk=pk)
+    if request.method == 'GET':
+        form = RecordForm(initial={
+            'author': record.author,
+            'email': record.email,
+            'text': record.text,
+        })
+        return render(request, 'record_update.html', {'record': record, 'form': form})
+    elif request.method == 'POST':
+        form = RecordForm(data=request.POST)
+        if form.is_valid():
+            record.author = form.cleaned_data.get('author')
+            record.text = form.cleaned_data.get('text')
+            record.email = form.cleaned_data.get('email')
+            record.save()
+            return redirect('index')
+        return render(request, 'record_update.html', {'record': record, 'form': form})
